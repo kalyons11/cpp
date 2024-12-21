036f4c4bc5e35baf99c2dfdc31e4e8856f6ad205
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-21 09:33:35 -0500

    Include .o extension when executing object file

--- run_cpp.sh
+++ run_cpp.sh
@@ -17,4 +17,4 @@ cd "$SOURCE_DIR" || { echo "Error: Cannot access directory $SOURCE_DIR"; exit 1;
 # Compile and run the program
 clang++ -mmacosx-version-min=13.0 -o "$BASE_NAME.o" -ggdb -O0 \
     -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion \
-    -Werror -std=c++20 "$BASE_NAME.cpp" && ./"$BASE_NAME"
+    -Werror -std=c++20 "$BASE_NAME.cpp" && ./"$BASE_NAME.o"
