1626f0d8b4c8baacf3825ceabef036b807ea917b
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-22 09:30:27 -0500

    Set up clang-format

--- hello/HelloWorld.cpp
+++ hello/HelloWorld.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
-int main()
-{
+int main() {
     std::cout << "Hello, world!\n";
 
     [[maybe_unused]] int x; // Definition then default-initialization
-    [[maybe_unused]] int y {}; // Definition then value-initialization (zero-initialization)
-    [[maybe_unused]] int z { 5 }; // Definition then direct-list-initialization - preferred
+    [[maybe_unused]] int
+        y{}; // Definition then value-initialization (zero-initialization)
+    [[maybe_unused]] int z{
+        5}; // Definition then direct-list-initialization - preferred
     [[maybe_unused]] int a = 2; // Definition then copy-initialization
     [[maybe_unused]] int b = 3, c = 5;
     // [[maybe_unused]] int d (); // Function definition of d returning int
-    [[maybe_unused]] int e (7); // Definition then direct-initialization
+    [[maybe_unused]] int e(7); // Definition then direct-initialization
 
     // [[maybe_unused]] int bad { 4.5 };
 
@@ -21,8 +22,8 @@ int main()
 
     // Using the extraction operator
     std::cout << "Enter two numbers separated by spaces: ";
-    int number {};
-    int other {};
+    int number{};
+    int other{};
     std::cin >> number >> other;
 
     std::cout << "You entered: " << number << "\n";
