119b0ca19d7804f714b226867021cf58bf6d52e3
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-24 14:24:53 -0500

    Add data size examples

---
+++ data/data.cpp
@@ -0,0 +1,39 @@
+#include <climits>  // for CHAR_BIT
+#include <iomanip>  // for std::setw (which sets the width of the subsequent output)
+#include <iostream>
+
+int main()
+{
+    // Fundamental data types
+    [[maybe_unused]] float f = 3.14159f;
+    [[maybe_unused]] double d = 4.56;
+    [[maybe_unused]] long double dd = 2.233;
+
+    [[maybe_unused]] bool b = true;
+
+    [[maybe_unused]] char c = 'c';
+    [[maybe_unused]] wchar_t cOther = 'd';
+    [[maybe_unused]] char8_t cX = 'e';
+
+    [[maybe_unused]] short int i = 65;
+    [[maybe_unused]] int another = 677;
+    [[maybe_unused]] long int extra = 1232113;
+
+    std::cout << "A byte is " << CHAR_BIT << " bits\n\n";
+
+    std::cout << std::left;  // left justify output
+
+    std::cout << std::setw(16) << "bool:" << sizeof(bool) << " bytes\n";
+    std::cout << std::setw(16) << "char:" << sizeof(char) << " bytes\n";
+    std::cout << std::setw(16) << "short:" << sizeof(short) << " bytes\n";
+    std::cout << std::setw(16) << "int:" << sizeof(int) << " bytes\n";
+    std::cout << std::setw(16) << "long:" << sizeof(long) << " bytes\n";
+    std::cout << std::setw(16) << "long long:" << sizeof(long long)
+              << " bytes\n";
+    std::cout << std::setw(16) << "float:" << sizeof(float) << " bytes\n";
+    std::cout << std::setw(16) << "double:" << sizeof(double) << " bytes\n";
+    std::cout << std::setw(16) << "long double:" << sizeof(long double)
+              << " bytes\n";
+
+    return 0;
+}
