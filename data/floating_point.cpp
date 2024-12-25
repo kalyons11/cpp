451896b605377eb1f3fa8a2033643a9badfb416c
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-24 21:08:04 -0500

    Add more float examples

--- data/floating_point.cpp
+++ data/floating_point.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "floating_point.h"
+#include <iostream>
 
 // Function to describe and print various floating point values
 void describe_floats()
@@ -10,12 +10,14 @@ void describe_floats()
     [[maybe_unused]] long double x = 23.2242;
 
     // Declare and initialize variables with different types
-    [[maybe_unused]] int a { 5 };      // 5 means integer
-    [[maybe_unused]] double b { 5.0 }; // 5.0 is a floating point literal (no suffix means double type by default)
-    [[maybe_unused]] float c { 5.0f }; // 5.0 is a floating point literal, f suffix means float type
+    [[maybe_unused]] int a{5};       // 5 means integer
+    [[maybe_unused]] double b{5.0};  // 5.0 is a floating point literal (no
+                                     // suffix means double type by default)
+    [[maybe_unused]] float c{
+        5.0f};  // 5.0 is a floating point literal, f suffix means float type
 
-    [[maybe_unused]] int z { 0 };      // 0 is an integer
-    [[maybe_unused]] double e { 0.0 }; // 0.0 is a double
+    [[maybe_unused]] int z{0};       // 0 is an integer
+    [[maybe_unused]] double e{0.0};  // 0.0 is a double
 
     // Print some floating point values
     std::cout << "Let's print some floating values." << '\n';
@@ -23,8 +25,8 @@ void describe_floats()
     std::cout << 6.7f << '\n';
     std::cout << 9876543.21 << '\n';
 
-    std::cout << 1.23e4 << '\n';  // Scientific notation
-    std::cout << 0.00123 << '\n'; // Small floating point number
-    std::cout << -3.14 << '\n';   // Negative floating point number
-    std::cout << 2.71828 << '\n'; // Another floating point number
+    std::cout << 1.23e4 << '\n';   // Scientific notation
+    std::cout << 0.00123 << '\n';  // Small floating point number
+    std::cout << -3.14 << '\n';    // Negative floating point number
+    std::cout << 2.71827 << '\n';  // Another floating point number
 }
