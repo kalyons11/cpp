9994de93c8a6c39a0d6a3a3e1d0499a17bad77e1
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-26 17:30:05 -0500

    Set up constant variables

---
+++ const_strings/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// Best practice: Don’t use const for value parameters, since it doesn’t provide
+// any benefit Also, don’t use const for return values, since it doesn’t provide
+// any benefit
+void printInt(const int value)
+{
+    std::cout << value << std::endl;
+}
+
+int main()
+{
+    // Make variables constant whenever possible
+    // Prefer constant variables over object-like macros with substitution text
+    const char* str = "Hello, World!";
+    std::cout << str << std::endl;
+
+    [[maybe_unused]] const double gravity{9.8};
+
+    std::cout << "Enter your age: ";
+    int age{};
+    std::cin >> age;
+
+    const int constAge{age};
+    std::cout << "Your age is: " << constAge << std::endl;
+    age = 30;  // This is allowed
+    // constAge = 40; // This is not allowed
+
+    printInt(5);
+    printInt(10);
+
+    return 0;
+}
