5bdd18fc279723a78eb62b11824cfdf4e9de9617
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-28 15:14:56 -0500

    Add basic usage of std::bitset

---
+++ bits/main.cpp
@@ -0,0 +1,29 @@
+#include <bitset>
+#include <iostream>
+
+int main()
+{
+    // 8 bits in size
+    [[maybe_unused]] std::bitset<8> bits{0b1100'0101};
+    std::cout << bits << '\n';
+    // We can use set, reset, and flip to change bits
+    bits.set(3);  // set bit 3 to 1
+    std::cout << bits << '\n';
+    bits.reset(4);  // set bit 4 to 0
+    std::cout << bits << '\n';
+    bits.flip(5);  // flip bit 5
+    std::cout << bits << '\n';
+
+    // We can use test to check the value of a bit
+    if (bits.test(3))
+    {
+        std::cout << "Bit 3 is set\n";
+    }
+
+    // There are other useful querying functions: size, count, any, none, all
+    std::cout << "The size of bits is: " << bits.size() << '\n';
+    std::cout << "The number of set bits is: " << bits.count() << '\n';
+    std::cout << "Are any bits set? " << std::boolalpha << bits.any() << '\n';
+    std::cout << "Are no bits set? " << std::boolalpha << bits.none() << '\n';
+    std::cout << "Are all bits set? " << std::boolalpha << bits.all() << '\n';
+}
