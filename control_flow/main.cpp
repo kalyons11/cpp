42478b4f820946c6431824e38946e006e9a989a4
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-29 12:27:42 -0500

    Fix bracing

--- control_flow/main.cpp
+++ control_flow/main.cpp
@@ -9,11 +9,7 @@ int main()
     std::cin >> number;
     std::cout << "You entered: " << number << '\n';
     if (number >= 10)
-        // {
         std::cout << "The number is greater than or equal to 10." << '\n';
-    // }
     else
-        // {
         std::cout << "The number is less than 10." << '\n';
-    // }
 }
