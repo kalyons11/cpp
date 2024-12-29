324db04c768375a164509c9adb4b4c3eadebe660
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-29 12:24:19 -0500

    Add basic conditionals as control flow

---
+++ control_flow/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Let's talk about control flow." << '\n';
+
+    std::cout << "Enter a number: ";
+    int number{};
+    std::cin >> number;
+    std::cout << "You entered: " << number << '\n';
+    if (number >= 10)
+        // {
+        std::cout << "The number is greater than or equal to 10." << '\n';
+    // }
+    else
+        // {
+        std::cout << "The number is less than 10." << '\n';
+    // }
+}
