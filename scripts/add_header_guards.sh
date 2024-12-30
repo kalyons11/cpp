a76b5561a1aab622af04353152a829585b46b26b
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-30 07:21:28 -0500

    Try to fix formatting

--- scripts/add_header_guards.sh
+++ scripts/add_header_guards.sh
@@ -21,3 +21,6 @@ done
 
 # Also rename to .hpp
 find . -type f -name '*.h' -exec bash -c 'git mv "$0" "${0%.h}.hpp"' {} \;
+
+# Replace any "include.h" with "include.hpp" in *.cpp files
+find . -type f -name "*.cpp" -exec sed -i 's/\.h"/.hpp"/g' {} \;
