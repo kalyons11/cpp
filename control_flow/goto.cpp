ead172e354f3956a389a190652ba506f8b0e5641
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-30 07:34:57 -0500

    Show goto with skip forward

--- control_flow/goto.cpp
+++ control_flow/goto.cpp
@@ -2,6 +2,18 @@
 
 #include "goto.hpp"
 
+// goto statements can also be used to jump forward
+void show_jump_forward(bool skip)
+{
+    if (skip)
+    {
+        std::cout << "skip is true, so we're skipping ahead.\n";
+        goto skipAhead;
+    }
+    std::cout << "This will only show if skip is false.\n";
+skipAhead:;  // null statement
+}
+
 void show_goto()
 {
     double x{};
@@ -16,4 +28,7 @@ tryAgain:
     }
 
     std::cout << "You entered " << x << ".\n";
+
+    show_jump_forward(true);
+    show_jump_forward(false);
 }
