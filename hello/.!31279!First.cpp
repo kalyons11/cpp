c05391433133b1fdb61f02a6f3f449b07067fd2f
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-30 07:44:01 -0500

    Show failure

---
+++ hello/.!31279!First.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+    // Evaluate the expression x * 2 rather than saving to some intermediate
+    // variable
+    std::cout << x << " * 2 = " << x * 2 << "\n";
+    std::cout << x << " * 3 = " << x * 3 << "\n";
+    return 0;
+}
