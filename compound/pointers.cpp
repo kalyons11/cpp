1a5b797c10d63e339b730bea6272d8e7346720b2
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-31 15:30:25 -0500

    Show de-referencing

--- compound/pointers.cpp
+++ compound/pointers.cpp
@@ -8,4 +8,8 @@ void start_pointers()
     int number = 10;
     std::cout << "Memory address of number: " << &number << '\n';
     std::cout << "Value of number: " << number << '\n';
+
+    // The dereference or indirection operator * is used to access the value
+    // stored at a memory address as an lvalue
+    std::cout << "Value at memory address: " << *&number << '\n';
 }
