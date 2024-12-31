f6db78737d7028f6371cd5f7675b93baa2a2403c
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-12-31 07:58:17 -0500

    Add non-type template parameters

--- again_funcs/templates.cpp
+++ again_funcs/templates.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 
 #include "templates.hpp"
@@ -53,6 +54,44 @@ void print_with_precision(std::string value, int precision) = delete;
 template <typename T, typename U>
 auto min(T a, U b) -> std::common_type_t<T, U>;
 
+// We can also use an abbreviated syntax
+auto equals(auto a, auto b)  // a and b can be different types
+{
+    return a == b;
+}
+
+template <typename T, typename U>
+auto min_new(T a, U b) -> std::common_type_t<T, U>
+{
+    return (a < b) ? a : b;
+}
+
+template <typename T, typename U, typename V>
+T min_new(T a, U b, V c)
+{
+    return min_new(min_new(a, b), c);
+}
+
+// We can use non-type template parameters
+template <int N>
+void print_n_times(const std::string& str)
+{
+    for (int i = 0; i < N; ++i)
+    {
+        std::cout << str << '\n';
+    }
+}
+
+// We can provide constexpr to the template function
+// template <double D>
+// double get_sqrt()
+// {
+//     static_assert(D >= 0, "D must be greater than or equal to 0");
+//     if constexpr (D >= 0)
+//         return std::sqrt(D);
+//     return 0.0;
+// }
+
 void do_templates()
 {
     // We can call the function with different types
@@ -76,6 +115,21 @@ void do_templates()
     std::cout << min(1.1, 2) << '\n';
     std::cout << min(1, 2) << '\n';
     std::cout << min(1.1, 2.2) << '\n';
+
+    std::cout << equals(1, 1) << '\n';
+    std::cout << equals(1.1, 1.1) << '\n';
+    std::cout << equals(1, 1.1) << '\n';
+
+    std::cout << min_new(1, 2) << '\n';
+    std::cout << min_new(1.1, 2) << '\n';
+    std::cout << min_new(1, 2.2) << '\n';
+    std::cout << min_new(1, 2, 3) << '\n';
+
+    print_n_times<5>("Hello");
+    print_n_times<3>("World");
+
+    // std::cout << get_sqrt<4.0>() << '\n';
+    // std::cout << get_sqrt<-1>() << '\n';  // This will not compile
 }
 
 // We can use multiple types in a template
