04285eb22d15d06498316dfb7364c5627e563cfe
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-01-08 07:19:02 -0500

    Add initial array usages

---
+++ array/intro.hpp
@@ -0,0 +1,6 @@
+#ifndef __ARRAY_INTRO_HPP_H
+#define __ARRAY_INTRO_HPP_H
+
+void demo_array();
+
+#endif  // __ARRAY_INTRO_HPP_H
---
+++ array/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "intro.hpp"
+
+int main()
+{
+    std::cout << "Hello, this is a sample message from project: " << "array"
+              << '\n';
+    demo_array();
+    return 0;
+}
