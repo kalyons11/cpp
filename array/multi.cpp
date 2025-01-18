a4437577cd01e4986560cca342112db71cd527e9
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2025-01-18 07:14:08 -0500

    Show usage with std::array

--- array/multi.cpp
+++ array/multi.cpp
@@ -2,6 +2,36 @@
 
 #include "multi.hpp"
 
+// We can have funcs take these in
+template <typename T, std::size_t Row, std::size_t Col>
+void printArray(const std::array<std::array<T, Col>, Row>& arr)
+{
+    for (const auto& arow : arr)  // get each array row
+    {
+        for (const auto& e : arow)  // get each element of the row
+            std::cout << e << ' ';
+
+        std::cout << '\n';
+    }
+}
+
+// An alias template makes this a lot less verbose
+template <typename T, std::size_t Row, std::size_t Col>
+using Array2D = std::array<std::array<T, Col>, Row>;
+
+template <typename T, std::size_t Row, std::size_t Col>
+void printAnother(const Array2D<T, Row, Col>& arr)
+{
+    for (const auto& row : arr)
+    {
+        for (const auto& val : row)
+        {
+            std::cout << val << ' ';
+        }
+        std::cout << '\n';
+    }
+}
+
 void show_multi_d()
 {
     [[maybe_unused]] int board[3][3]{};  // We can have 2D arrays
@@ -33,4 +63,18 @@ void show_multi_d()
         }
         std::cout << '\n';
     }
+
+    // We can have a 2+D std::array
+    std::array<std::array<int, 4>, 3> arr{{// note double braces
+                                           {1, 2, 3, 4},
+                                           {5, 6, 7, 8},
+                                           {9, 10, 11, 12}}};
+    printArray(arr);
+
+    Array2D<int, 2, 2> another{{{1, 2}, {3, 4}}};
+    printAnother(another);
+
+    Array2D<double, 3, 3> doubleArray{
+        {{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}, {7.7, 8.8, 9.9}}};
+    printAnother(doubleArray);
 }
